af72006d74a71992f3838dcd464ffcd083cb49e2
Laksana Sriharan <107987128+Laksana-Sriharan@users.noreply.github.com>
2023-01-03 14:30:47 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,11 +1,11 @@
 class Sum{
     int a = 2;
     int b = 3;
-<<<<<<< HEAD
+
     int c =4;
-=======
+
     int c =8;
->>>>>>> 92ae7d57ccd6607a00211e73e79de7014a4e0433
+
     int d=9;
     System.out.println(a+b);
 }
